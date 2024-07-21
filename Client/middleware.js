48e7a739ae524c768a693d53b6f9d31c4327cac5
sagarpandey141 <121431268+sagarpandey141@users.noreply.github.com>
2024-07-21 09:02:01 +0530

    final commit refresh

--- Client/middleware.js
+++ Client/middleware.js
@@ -1,16 +1,7 @@
-// import { withMiddlewareAuthRequired } from "@auth0/nextjs-auth0/edge";
-
-// export default withMiddlewareAuthRequired();
-
-// export const config = {
-//   matcher: ["/middleware", "/api/data"],
-// };
-
-// middleware.js
 import { withMiddlewareAuthRequired } from "@auth0/nextjs-auth0/edge";
 
 export default withMiddlewareAuthRequired();
 
 export const config = {
-  matcher: ["/api/protected-data"], // Ensure these paths do not overlap with edge function paths
+  matcher: ["/middleware", "/api/data"],
 };
